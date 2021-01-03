ecf69b35395f043bcdb3f331ee7df7338a2e2a6c
BsShant <iambishant@gmail.com>
2021-01-03 21:34:17 +0545

    so

--- module/auth/auth.service.js
+++ module/auth/auth.service.js
@@ -1,5 +1,5 @@
 const userModel = require('../user/user.model');
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 
 const createUser = async (data) =>{
