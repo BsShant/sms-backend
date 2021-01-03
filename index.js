194992671d5aa8ea338c329aafea1682b2f73385
BsShant <iambishant@gmail.com>
2021-01-03 20:16:50 +0545

    changes

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require ('express');
 const app = express();
 const indexRouter = require("./routes/index");
-const port=5000;
 const cors = require('cors');
 const mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost:27017/mongo');
 
-
+app.set('port', process.env.PORT || 5000);
 app.use(express.json());
 app.use(cors());
 app.use(function (req, res, next) {
